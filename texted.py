762f2ba3a8ded26fd0253c1706a347432497f8aa
icedwater <icedwater@gmail.com>
2021-10-07 21:58:28 +0800

    Added basic status bar and two file menu items.

--- texted.py
+++ texted.py
@@ -6,7 +6,29 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200, 100))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
+
+        filemenu = wx.Menu()
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", "Information about the program")
+        filemenu.AppendSeparator()
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Leave the program")
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
+
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+
         self.Show(True)
+
+    def OnAbout(self, event):
+        dlg = wx.MessageDialog(self, "Not much to do here.", "About text ed", wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnExit(self, event):
+        self.Close(True)
 
 app = wx.App(False)
 frame = MyFrame(None, "text ed")
