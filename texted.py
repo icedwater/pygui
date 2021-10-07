aeb04e2d99c6d6f6f75f0e2f23eacc492608ac7e
icedwater <icedwater@gmail.com>
2021-10-07 22:43:54 +0800

    Added Open and Save to text ed.

--- texted.py
+++ texted.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import wx
+import os
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, title):
@@ -10,6 +11,8 @@ class MyFrame(wx.Frame):
 
         filemenu = wx.Menu()
         menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", "Information about the program")
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", "Open a file")
+        menuSave = filemenu.Append(wx.ID_SAVE, "&Save", "Save a file")
         filemenu.AppendSeparator()
         menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Leave the program")
 
@@ -17,14 +20,35 @@ class MyFrame(wx.Frame):
         menuBar.Append(filemenu, "&File")
         self.SetMenuBar(menuBar)
 
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        self.Bind(wx.EVT_MENU, self.OnSave, menuSave)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 
         self.Show(True)
 
     def OnAbout(self, event):
-        dlg = wx.MessageDialog(self, "Not much to do here.", "About text ed", wx.OK)
+        dlg = wx.MessageDialog(self, "Edit, open, and save.", "About text ed", wx.OK)
         dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnOpen(self, event):
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, '', "*.*", wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            with open(os.path.join(self.dirname, self.filename), 'r') as openfile:
+                self.control.SetValue(openfile.read())
+        dlg.Destroy()
+
+    def OnSave(self, event):
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Save to file", self.dirname, '', "*.*", wx.FD_SAVE)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            self.control.SaveFile(filename=os.path.join(self.dirname, self.filename))
         dlg.Destroy()
 
     def OnExit(self, event):
